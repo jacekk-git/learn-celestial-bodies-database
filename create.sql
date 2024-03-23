5800b0851d19d5e3519aa6911f8a77aa2c15449b
Jacek Kucharski <jacekk@op.pl>
2024-03-23 23:19:57 +0000

    create db script

--- create.sql
+++ create.sql
@@ -1,3 +1,6 @@
+drop table if exists comet;
+drop table if exists moon;
+drop table if exists planet;
 drop table if exists star;
 drop table if exists galaxy;
 
@@ -17,14 +20,77 @@ create table  star(
 	star_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
 	galaxy_id INT NOT NULL,
 	name VARCHAR(255) UNIQUE,
+	description TEXT,
+	is_dying BOOL,
 	PRIMARY KEY(star_id),
 	CONSTRAINT fk_galaxy
 		FOREIGN KEY(galaxy_id)
 			REFERENCES galaxy(galaxy_id)
 );
 
---create table  planet(
---);
+create table  planet(
+	planet_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+	star_id INT NOT NULL,
+	name VARCHAR(255) UNIQUE,
+	description TEXT,
+	radius_in_km INT NOT NULL,
+	PRIMARY KEY(planet_id),
+	CONSTRAINT fk_star
+		FOREIGN KEY(star_id)
+			REFERENCES star(star_id)
+);
 
---create table  moon(
---);
+create table  moon(
+	moon_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+	planet_id INT NOT NULL,
+	name VARCHAR(255) UNIQUE,
+	radius_in_km INT NOT NULL,
+	is_single BOOL NOT NULL,
+	PRIMARY KEY(moon_id),
+	CONSTRAINT fk_planet
+		FOREIGN KEY(planet_id)
+			REFERENCES planet(planet_id)
+);
+
+create table comet(
+	comet_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255) UNIQUE,
+	age_in_thousand_years INT,
+	PRIMARY KEY (comet_id)
+);
+
+insert into galaxy(name, number_of_stars, age_in_millions_of_years, radius_in_thousands_km, description, is_far)
+values
+	('Galaxy 1', 1, 1234, 1111, 'Description 1', false)
+, ('Galaxy 2', 1, 2222, 2211, 'Description 2', true)
+, ('Galaxy 3', 2, 3333, 3322, 'Description 2', true)	
+, ('Galaxy 4', 1, 4444, 4433, 'Description 2', true)	
+, ('Galaxy 5', 1, 5555, 5544, 'Description 2', true)	
+, ('Galaxy 6', 1, 6666, 6655, 'Description 2', false)
+;
+
+insert into star(galaxy_id, name)
+select galaxy_id, concat(name, ' - star 1') 
+from galaxy;
+
+insert into planet(star_id, name, radius_in_km)
+select star_id, concat(name, ' - planet 1'), star_id * 1000
+from star 
+union all 
+select star_id, concat(name, ' - planet 2'), star_id * 2000
+from star;
+
+insert into moon(planet_id, name, radius_in_km, is_single)
+select planet_id, concat(name, ' - moon 1'), planet_id * 456, false
+from planet
+union all
+select planet_id, concat(name, ' - moon 2'), planet_id * 555, false
+from planet
+;
+
+insert into comet(name, age_in_thousand_years)
+values
+	('Comet 1', 12345)
+, ('Comet 2', 22222)
+, ('Comet 3', 54322)	
+;
